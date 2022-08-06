cf9992aa946df5bc9ef9da716c0273fa4c2ae67a
LeetTenki <boshoabotsoere7@gmail.com>
2022-08-06 08:42:38 +0200

    I made the board for the game

--- src/components/board.js
+++ src/components/board.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Box from './box'
-//Bellow is the styling of the board
+//Bellow is the styling of the board.
 const style = {
 	width: "250px",
 	height: "250px",
@@ -8,8 +8,7 @@ const style = {
 	display: "grid",
 	gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
 };
-
-//Bellow is the board with 9 squares or Boxes
+//Bellow is the board with 9 squares or Boxes.
 const Board = (props) => (
     <div style={style}>
         {[ ...Array(9)].map((_, pos) => <Box key={pos} name={pos} onClick={()=>props.onClick(pos)} value={props.value[pos]}/>)}
